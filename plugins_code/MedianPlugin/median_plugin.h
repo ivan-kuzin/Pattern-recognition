74d3306c8e8d48c6b90ee4a3aefbc4ff37d0a5f1
ivan-kuzin <56124096+ivan-kuzin@users.noreply.github.com>
2022-04-08 11:52:44 +0300

    Лаб 2

---
+++ plugins_code/MedianPlugin/median_plugin.h
@@ -0,0 +1,20 @@
+#ifndef MEDIAN_PLUGIN_H
+#define MEDIAN_PLUGIN_H
+
+#include <QObject>
+#include <QtPlugin>
+#include "editor_plugin_interface.h"
+
+class median_plugin: public QObject, public EditorPluginInterface
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID EDIT_PLUGIN_INTERFACE_IID);
+    Q_INTERFACES(EditorPluginInterface);
+public:
+    QString name();
+    void edit(const cv::Mat &input, cv::Mat &output);
+private:
+    void shellsort(int* l, int* r);
+};
+
+#endif // MEDIAN_PLUGIN_H
